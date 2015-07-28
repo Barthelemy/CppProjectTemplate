ff055e9d4fe8d635eda0810cf1282bc05494bbb4
bvonhall <barthelemy.von.haller@cern.ch>
2015-07-28 17:14:11 +0200

    dependency between projects

---
+++ ProjA/cmake/ProjAConfig.cmake
@@ -0,0 +1,2 @@
+include("${CMAKE_CURRENT_LIST_DIR}/ProjATargets.cmake")
+#include("${CMAKE_CURRENT_LIST_DIR}/@targets_export_name@.cmake")
---
+++ ProjB/cmake/ProjBConfig.cmake
@@ -0,0 +1,2 @@
+include("${CMAKE_CURRENT_LIST_DIR}/ProjBTargets.cmake")
+#include("${CMAKE_CURRENT_LIST_DIR}/@targets_export_name@.cmake")
